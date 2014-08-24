ce0f3d01a2026f94d3123565b1deed59240176fb
sumdp <daniel.pedraza@me.com>
2014-08-24 16:34:05 -0700

    fixed typos

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,3 @@
-```
-
 #import dplyr library - makes life easier!
 library(dplyr)
 #import reshape2 library - thank you Hadley Wickham!
@@ -16,7 +14,7 @@ subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", quote="\""
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", quote="\"") # identifies the subject who performed the activity  in trainig data
 
 #Label features with appropriate labels: features come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ
-names(features) <- c("featureNo.", "sensor_measurement_description")
+names(features) <- c("featureNo.", "sensor_measurement_description") 
 #Provide descriptive column names for training and test data (from feature labels)
 names(X_test) <- features$sensor_measurement_description
 names(X_train) <- features$sensor_measurement_description
@@ -40,7 +38,7 @@ test_data <- cbind(X_test, subject_test, y_test_labelled)
 #Combine the two dataframes into single dataframe including Training & Testing datasets: X_train & X_test (includes activity labels/descriptors)
 complete_data <- rbind(training_data, test_data)
 
-#Find variable names to extract - select only columns including: "mean()" - Mean value and "std()" - Standard deviation for signals needed to estimate variables of the feature vector.
+#Find variable names to extract - select only columns including: "mean()" - Mean value and "std()" - Standard deviation for signals needed to estimate variables of the feature vector. 
 grep("mean\\(\\)|std\\(\\)", names(complete_data), value=TRUE)
 #Select columns from dataset which match specified grep commands (subset on mean & std)
 #Excluding:
@@ -54,13 +52,11 @@ namesVector <- gsub("\\(\\)","",namesVector)
 namesVector <- gsub("-","_",namesVector)
 #Update variable names that are now clean & descriptive
 names(extracted_columns) <- namesVector
-#Optional: Tabulate data to see which subject performed what activity (thi helps me to see where I stand and observe the data)
+#Optional: Tabulate data to see which subject performed what activity (this helps me to see where I stand and observe the data)
 table(extracted_columns$subject, extracted_columns$activity_description)
 #Melt dataframe by subject and activity description
 meltdata <- melt(extracted_columns, id=c("subject", "activity_description"))
-#Cast data in tidy format
+#Cast data in tidy format 
 cdata <- dcast(meltdata, subject + activity_description ~ variable, mean)
 #Write file with tidy data to upload as submission of Course Project for Getting and Cleaning Data - Coursera
 write.table(cdata, file = "average_tidy_data.txt", row.name=FALSE)
-
-```
